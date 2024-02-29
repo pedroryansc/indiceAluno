b7b33c1a05152584e75403cd6068111a1a83b6e5
pedroryansc <pedroryangamer@gmail.com>
2024-02-29 16:38:07 -0300

    Início da atividade termômetro de Desenvolvimento Web I

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Índice de Aproveitamento de Aluno</title>
+</head>
+<body>
+    <h2>Cadastro de aluno:</h2>
+    <form method="post">
+        Nome: <br><input type="text" name="nome"><br>
+        <br>
+        Idade: <br><input type="text" name="idade"><br>
+        <br>
+        Índice de Aproveitamento do Aluno (IAA): <br><input type="text" name="iaa"><br>
+        <br>
+        Curso: <br><input type="text" name="curso"><br>
+        <br>
+        <button type="submit" name="acao" value="salvar">Salvar</button>
+    </form>
+</body>
+</html>
